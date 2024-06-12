a197a3335bf7624b5f6ea66b6bd07c5a9e060469
Shanmukh459 <saikrrish459@gmail.com>
2024-06-12 11:56:23 -0700

    added content for header - need to style and refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import logo from "./assets/logo.svg"
+import home from "./assets/home.svg"
+import patient from "./assets/patient.svg"
+import calendar from "./assets/calendar.svg"
+import message from "./assets/message.svg"
+import transaction from "./assets/transaction.svg"
+import jose from "./assets/joseSimmons.png"
+import settings from "./assets/settings.svg"
+import more from "./assets/more.svg"
+const App = () => {
   return (
-    <>
+    <header>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={ logo }/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <div>
+          <img src={ home }/>
+          <span>Overview</span>
+        </div>
+        <div>
+          <img src={ patient }/>
+          <span>Patients</span>
+        </div>
+        <div>
+          <img src={ calendar } />
+          <span>Schedule</span>
+        </div>
+        <div>
+          <img src={ message } />
+          <span>Message</span>
+        </div>
+        <div>
+          <img src={ transaction } />
+          <span>Transactions</span>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div>
+        <img src={jose} />
+        <div>
+          <p>Dr.Jose Simmons</p>
+          <p>General Practitioner</p>
+          <img src={settings} />
+          <img src={more} />
+        </div>
+      </div>
+
+    </header>
   )
 }
 
