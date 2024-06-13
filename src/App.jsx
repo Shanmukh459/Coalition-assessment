2b26a0341713d624e0f0957d766c76320234f8c8
Shanmukh459 <saikrrish459@gmail.com>
2024-06-13 12:44:57 -0700

    header completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Header from './components/Header'
 
 const App = () => {
   return (
-    <Header />
+    <div className='app-container'>
+      <Header />
+    </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import Icon from '../utils/Icon'
+import React from "react"
+import Icon from "../utils/Icon"
+import "../assets/styles/header.css"
 
 const Header = () => {
   return (
@@ -7,38 +8,39 @@ const Header = () => {
       <div>
         <Icon icon="logo" />
       </div>
-      <div>
+      <div className="header-center">
         <div>
-        <Icon icon="home" />
+          <Icon icon="home" />
           <span>Overview</span>
         </div>
-        <div>
-        <Icon icon="patient" />
+        <div className="header-center-selected">
+          <Icon icon="patient" />
           <span>Patients</span>
         </div>
         <div>
-        <Icon icon="calendar" />
+          <Icon icon="calendar" />
           <span>Schedule</span>
         </div>
         <div>
-        <Icon icon="message" />
+          <Icon icon="message" />
           <span>Message</span>
         </div>
         <div>
-        <Icon icon="transaction" />
+          <Icon icon="transaction" />
           <span>Transactions</span>
         </div>
       </div>
-      <div>
-      <Icon icon="jose" />
-        <div>
-          <p>Dr.Jose Simmons</p>
-          <p>General Practitioner</p>
+      <div className="header-right">
+        <Icon icon="jose" />
+        <div className="header-right-right">
+          <div className="header-right-info">
+            <p className="name">Dr.Jose Simmons</p>
+            <p className="title">General Practitioner</p>
+          </div>
           <Icon icon="settings" />
           <Icon icon="more" />
         </div>
       </div>
-
     </header>
   )
 }
